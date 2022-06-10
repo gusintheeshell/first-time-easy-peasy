c62fbfc131fa259ccccb75cd4c389e987f9c8c4c
Gustavo Barbosa <gusbdev@gmail.com>
2022-06-10 16:20:54 -0300

    UI part

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import './App.css';
 
+import {useStoreActions, useStoreState} from "./store/store"
+import {CharacterType} from "./store/model/character/character"
+
 function App() {
+  const {getCharacter} = useStoreActions((actions) => actions.characters);
+  const {characters} = useStoreState((state) => state.characters);
+  const callAPI = useCallback(() => getCharacter(), [characters]);
+  
+  useEffect(() => {
+    callAPI()
+    console.log('i fire once');
+  }, []);
+
   return (
     <div className="App">
+      <ul>
+        {characters[0].map((char: CharacterType) => (
+          <li>{char.name} - {char.birth_year}</li>
+        ))}
+      </ul>
     </div>
   );
 }
