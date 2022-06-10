170da016f92266387bb228201f4e42afaa0dd749
Gustavo Barbosa <gusbdev@gmail.com>
2022-06-10 16:20:07 -0300

    Thunk function

--- src/store/model/character/character.ts
+++ src/store/model/character/character.ts
@@ -1,19 +1,34 @@
-import {Action, action} from "easy-peasy"
+import {Action, action, Thunk, thunk} from "easy-peasy"
 
-interface CharacterType {
+import api from "../../../services/api";
+
+export interface CharacterType {
     name: string;
     birth_year: string;
 }
 
 export interface CharacterModel {
-    characters: Array<CharacterType>,
-    add: Action<CharacterModel, CharacterType>
+    characters: Array<any>,
+    addCharacter: Action<CharacterModel, Array<any>>,
+    getCharacter: Thunk<CharacterModel>
 }
 
 const characters: CharacterModel = {
     characters: [],
-    add: action((state, payload: CharacterType) => {
+    addCharacter: action((state, payload: Array<any>) => {
         state.characters.push(payload)
+    }),
+    getCharacter: thunk(async (actions) => {
+        const {data} = await api.get("people/");
+        const {results} = data;
+        // const newCharacters: Array<CharacterType> = [];
+        // results.forEach(({name, birth_year}: CharacterType) => {
+        //     newCharacters.push({
+        //         name,
+        //         birth_year
+        //     })
+        // })
+        actions.addCharacter(results)
     })
 }
 
